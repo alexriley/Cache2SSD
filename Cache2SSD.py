613de5fb1254380adb2ccd646bf8c2fb49384a58
Alex Riley <alex@riley.cc>
2015-02-21 17:22:20 -0500

    Now checks for a config file. Uncache written but not tested. Cache now a function

--- Cache2SSD.py
+++ Cache2SSD.py
@@ -1,32 +1,137 @@
 #/bin/python3
+'''
+Cache2SSD.py
+Written by Alex Riley <alex@riley.cc>
+Created 21 February 2015
+Last Modified 21 February 2015
 
+'''
 from os.path import isdir, exists
 
 import sys, os, shutil
+
+#Globals:
+errors = []
+SourcesArgList = {'-s','-src','-source'}
+DestinationArgList = {'-d','-dest','-destination'}
+SourceDirectory = ''
+DestinationDirectory = ''
+ConfigFileName = '.Cache2SSD.config'
+ConfigFile = open(ConfigFileName, "r+")
+    
 def PromptSourceDirectory():
     return input("Where would you like to cache from? (e.g. 'Your steam folder'/steamapps/common)\n->")
 def PromptDestDirectory():
-    return input("Where to cache this to? (Select any directory on the SSD which you have write permissions for)\n->")
+    return input("Where to cache this to? (Select any directory on the SSD which you have read/write permissions for)\n->")
+
+def ReadConfigFile():
+    #Returns tuple if 
+    ConfigFile.seek(0,0) # go to the beginning of the file
+    line=ConfigFile.readline()
+    SourceDirectory = ''
+    DestinationDirectory = ''
+    #Parse file:
+    while line:
+        if line[0] != '#': #for comments
+            if "SOURCE=" in line:
+                if SourceDirectory != '':
+                    print("Bad Config file, multiple Sources specified. Config file Ignored")
+                    return ('','')
+                SourceDirectory = line.replace("SOURCE=","").replace("\n","")
+            elif "DESTINATION=" in line:
+                if DestinationDirectory != '':
+                    print("Bad Config file, multiple Sources specified. Config file Ignored")
+                    return ('','')
+                DestinationDirectory = line.replace("DESTINATION=","").replace("\n","")
+            line = ConfigFile.readline()
+    #Check values:
+    for x in {SourceDirectory, DestinationDirectory}:
+        if not exists(x):
+            print("Directory %s in config file does not exist, Config file ignored" % (x))
+            return ('','')
+    return (SourceDirectory,DestinationDirectory)
+        
+                
+        
+        
 def cache(Source, Destination):
-    #TODO
-    return
+    #Copy the directory from source to destination
+    if exists(Destination):
+        print("ERROR: %s already exists." % Destination)
+        return False
+
+    try:
+        shutil.move(Source,Destination)
+    except OSError as why:
+        errors.extend(str(why))
+    if errors:
+        print("Directory not copied\nERRORS:")
+        for x in errors:
+            print("%s", x)
+        return False
+    
+    #Create symbolic link
+    if(exists(Source)):
+        print("%s Should have been moved, but wasn't",Source)
+        return False
+    
+    ShellSymLink = "ln -s 'DESTINATIONPATH' 'SOURCEPATH'"
+    ShellSymLink = ShellSymLink.replace("DESTINATIONPATH", Destination)
+    ShellSymLink = ShellSymLink.replace("SOURCEPATH", Source)
+    
+    print("Shell command:\n%s" % (ShellSymLink)) #TESTING
+    
+    os.popen(ShellSymLink)
+    return True
 
 def uncache(Source, Destination):
     #TODO
-    return
+    '''
+    Source is a symlink to Destination
+    
+    Pseudocode:
+    Check that Source is a symlink. Return false if not
+    
+    Otherwise:
+    Remove source symlink
+    Move Destination to Source
+    Delete Destination
+    return true
+    '''  
+    if not os.path.islink(Source):
+        return False
+    
+    RemoveSymLinkCommand = "rm -f 'SOURCE'" # NOTE: this removes the symlink, it doesn't delete any files
+    RemoveSymLinkCommand = RemoveSymLinkCommand.replace("SOURCE", Source)
+    os.popen(RemoveSymLinkCommand)
+    
+    if(exists(Source)):
+        print("%s Should have been moved, but wasn't",Source)
+        return False
+    
+    try:
+        shutil.move(Destination, Source)
+    except OSError as why:
+        errors.extend(str(why))
+    if errors:
+        print("Directory not copied\nERRORS:")
+        for x in errors:
+            print("%s", x)
+        return False
+    
+    return True
     
 def main():
-    SourcesArgList = {'-src','-s','-source'}
-    DestinationArgList = {'-d','-dest','-destination'}
-    SourceDirectory = ''
-    DestinationDirectory = ''
-    errors = []
+    (SourceDirectory,DestinationDirectory) = ReadConfigFile()
+    # If there are arguments, parse them for commands:
     if len(sys.argv) > 1:
         for x in range(1,len(sys.argv)-1):
-            if sys.argv[x] in SourcesArgList:
+            if sys.argv[x] in SourcesArgList and SourceDirectory == '':
                 SourceDirectory = sys.argv[x+1]
-            if sys.argv[x] in DestinationArgList:
+            if sys.argv[x] in DestinationArgList and DestinationDirectory == '':
                 DestinationDirectory = sys.argv[x+1]
+                
+    # If prompt for directories if not specified in arguments
     if SourceDirectory == '': SourceDirectory = PromptSourceDirectory()
     if DestinationDirectory == '': DestinationDirectory = PromptDestDirectory()
 
@@ -48,54 +153,25 @@ def main():
     SourceChoice = -1
     dirs = os.listdir(SourceDirectory)
     dirs.sort()
-    
     if(len(dirs) == 0):
         print("Source directory %s is empty, exiting" % (SourceDirectory))
         return
     #Display list of choices
     # Note: Starting the count at 1 for the user, adjusting the users input accordingly
     # (If user selects '7' from the list shown, it's actually dirs[6])
-    if SourceDirectory != '':
-        print("dirs length: %i" % (len(dirs)))
-        for x in range(len(dirs)):
-            print("%i: %s" % (x+1,dirs[x])) 
-        SourceChoice = int(input("What would you like to cache? ->")) -1
-        if SourceChoice <= 0 or SourceChoice >= len(dirs):
-            print("%i is not a valid choice" % (SourceChoice+1))
-            return
+    for x in range(len(dirs)):
+        print("%i: %s" % (x+1,dirs[x])) 
+    SourceChoice = int(input("What would you like to cache? ->")) -1
+    if SourceChoice <= 0 or SourceChoice >= len(dirs):
+        print("%i is not a valid choice" % (SourceChoice+1))
+        return
     SourcePath = SourceDirectory+dirs[SourceChoice]
     DestinationPath = DestinationDirectory+dirs[SourceChoice]
-    if exists(DestinationPath):
-        print("ERROR: %s already exists." % DestinationPath)
-        return
-    
-    print("Copying %s to %s" % (SourcePath,DestinationPath))
-    
-    #Copy the directory from source to destination
-    try:
-        shutil.move(SourcePath,DestinationPath)
-    except OSError as why:
-        errors.extend(str(why))
-    if errors:
-        print("Directory not copied\nERRORS:")
-        for x in errors:
-            print("%s", x)
-        return
-    
-    #Create symbolic link
-    if(exists(SourcePath)):
-        print("%s Should have been moved, but wasn't",SourcePath)
-        return
-    
-    ShellSymLink = "ln -s DESTINATIONPATH SOURCEPATH"
-    ShellSymLink = ShellSymLink.replace("DESTINATIONPATH", DestinationPath)
-    ShellSymLink = ShellSymLink.replace("SOURCEPATH", SourcePath)
-    
-    print("Shell command:\n%s" % (ShellSymLink)) #TESTING
-    
-    os.popen(ShellSymLink)
-    input("COMPLETE %s is now cached. Press enter to exit." % (SourcePath))
-    
+
+    if cache(SourcePath,DestinationPath):
+        input("COMPLETE %s is now cached. Press enter to exit." % (SourcePath))
+    else :
+        input("Not cached. Press enter to exit")
     
     
     
