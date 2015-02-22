65a4e66187843909259d31d08c48afbc505fab3d
Alex Riley <alex@riley.cc>
2015-02-22 15:07:32 -0500

    Moved nearly everything to class. Some code cleaned up.

--- Cache2SSD.py
+++ Cache2SSD.py
@@ -1,9 +1,9 @@
 #/bin/python3
 '''
 Cache2SSD.py
-Written by Alex Riley <alex@riley.cc>
+Originally written by Alex Riley <alex@riley.cc>
 Created 21 February 2015
-Last Modified 21 February 2015
+Last Modified 22 February 2015
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -35,7 +35,59 @@ class Cache2SSD:
     #The cached file is used to keep track of which folders are being cached on a drive
     #The file consists of folder names, separated by newlines. It's stored in the cache directory
     CachedFilesName= '.Cache2SSD.Cachedlist' 
-     
+    def RunCLImode(self):
+        # If prompt for directories if not specified in arguments
+        if self.Source == '': self.Source = self.PromptSourceDirectory()
+        if self.Cache == '': self.Cache = self.PromptCacheDirectory()
+    
+        print("Source %s\nCache: %s" % (self.Source,self.Cache)) #TESTING
+        for directory in {self.Source, self.Cache}:  
+            if exists(directory):
+                if not isdir(directory): 
+                    print("ERROR: %s is not a directory" % (directory))
+                    return
+            else:
+                #TODO: Prompt user if they would like to create the directory
+                print("ERROR: %s does not exist. Create it using mkdir or a file manager" % (directory))
+                return 
+            
+        if not (self.Source.endswith('/')):
+            self.Source += '/'
+        if not (self.Cache.endswith('/')):
+            self.Cache += '/'
+        
+        SourceFilesList = []
+        CachedFilesList = []
+        (SourceFilesList,CachedFilesList) = self.GetFilesList()
+        if(len(SourceFilesList) == 0 and len(CachedFilesList) == 0):
+            print("Source directory %s is empty, exiting" % (self.Source))
+            return
+        #Display list of choices
+        # Note: Starting the count at 1 for the user, adjusting the users input accordingly
+        
+        #Choice = int(input("What would you like to do? ->")) -1
+        Choice = self.PromptForChoice(SourceFilesList, CachedFilesList)
+        if Choice < 0 or Choice > len(SourceFilesList)+len(CachedFilesList):
+            print("%i is not a valid choice" % (Choice+1))
+            return
+        elif(Choice < len(SourceFilesList)): # If Choice is among cached
+            SourcePath = self.Source+SourceFilesList[Choice]
+            CachePath = self.Cache+SourceFilesList[Choice]
+            if self.cache(SourcePath,CachePath):
+                print("COMPLETE %s is now cached." % (SourcePath))
+            
+            else :
+                print("ERROR: Not cached.")
+          
+        elif(Choice >= len(SourceFilesList)): # If choice is to uncache
+            Choice -= (len(SourceFilesList))
+            SourcePath = self.Source+CachedFilesList[Choice]
+            CachePath = self.Cache+CachedFilesList[Choice]
+    
+            if self.uncache(SourcePath, CachePath):
+                print("COMPLETE %s returned." % (CachePath))
+            else:
+                print("Not cached.")
     def SetSourceDirectory(self,SourceDirectory):
         self.Source = SourceDirectory
     def GetSourceDirectory(self): return self.Source
@@ -73,8 +125,8 @@ class Cache2SSD:
         return(SourceFilesList,CachedFilesList)
          
     def ReadConfigFile(self):
+        #Returns tuple of (Source, Cache), '' if one is not found
         ConfigFile = open(self.ConfigFileName, "a+")
-        #Returns tuple if 
         ConfigFile.seek(0,0) # go to the beginning of the file
         line=ConfigFile.readline()
         SourceDirectory = ''
@@ -106,18 +158,19 @@ class Cache2SSD:
                     print("Directory %s in config file does not exist. Config file ignored" % (x))
                     return ('','')
         ConfigFile.close()
-        return (SourceDirectory,CacheDirectory)
-             
-                     
-             
+        if SourceDirectory != '': self.SetSourceDirectory(SourceDirectory)
+        if CacheDirectory != '': self.SetCacheDirectory(CacheDirectory)
+        #return (SourceDirectory,CacheDirectory)
              
     def cache(self,Source,Cache):
         #Source is the folder in SourceDirectory which will be copied to Cache
+        
+        
         #FolderName = Cache.replace(CacheDirectory,"") #String of the folder user selected (e.g. Half-Life)
         
         #FolderName is everything in Cache after the last occurrence of '/' to the end:
         #e.g. if cache is /BigDrive/Half-Life, Folder name is Half-Life
-        FolderName = Cache[Cache[0:len(Cache)-1].rindex("/")+1:] #ADDED, TEST THIS
+        FolderName = Cache[Cache[0:len(Cache)-1].rindex("/")+1:]
         #Copy the directory from source to Cache    
         if exists(Cache):
             print("ERROR: %s already exists." % Cache)
@@ -144,7 +197,7 @@ class Cache2SSD:
         subprocess.call(ShellSymLink)
          
         #Add this file to the list of cached files
-        CacheFile = open(CacheDirectory+self.CachedFilesName,'a')
+        CacheFile = open(self.Cache+self.CachedFilesName,'a')
         CacheFile.write(FolderName+'\n')
         CacheFile.close()
          
@@ -189,7 +242,7 @@ class Cache2SSD:
             return False
         #Update the cache list file
         #Get all lines from the file
-        CacheFile = open(CacheDirectory+self.CachedFilesName, 'r')
+        CacheFile = open(self.Cache+self.CachedFilesName, 'r')
         CacheFileLines = []
         line = CacheFile.readline()
         while line:
@@ -199,99 +252,52 @@ class Cache2SSD:
         
         CacheFileLines.sort()
         #Add back all lines except for the one just uncached
-        CacheFile = open(CacheDirectory+self.CachedFilesName,'w')
+        CacheFile = open(self.Cache+self.CachedFilesName,'w')
         for line in CacheFileLines:
             if line.replace("\n","") != CachedFolderName:
                 CacheFile.write(line)
         CacheFile.close()
         
         return True
-        
-def main():
-    global SourceDirectory
-    global CacheDirectory
+    def PromptForChoice(self,SourceFilesList,CachedFilesList):
+        print("Source Directories (Enter number to cache):")
+        if len(SourceFilesList) == 0: print("\tNone")
+        for x in range(len(SourceFilesList)):
+            print("%i: %s" % (x+1,SourceFilesList[x])) 
+        print("Cached Directories (Enter number to uncache):")
+        if(len(CachedFilesList) == 0): print("\tNone")
+        for x in range(len(CachedFilesList)):
+            print("%i: %s" % (len(SourceFilesList)+x+1,CachedFilesList[x]))
+        Choice = int(input("What would you like to do? ->")) -1
+        return Choice
     
-    SourcesArgList = {'-s','-src','-source'}
-    CacheArgList = {'-c','-cache'}
-    SSDCache = Cache2SSD()
+
+def main():
     if platform.system() != 'Linux':
         ans = raw_input("WARNING: Cache2SSD has only been tested on Linux. It may work on Mac OS X (as it is POSIX-compliant), but won't work on Windows. Would you like to continue (yes/no)? \n->")
         if ans not in {'y','yes'}:
             return
-    (SourceDirectory,CacheDirectory) = SSDCache.ReadConfigFile()
+    SSDCache = Cache2SSD()
+    SSDCache.ReadConfigFile()
+    (Source,Cache) = GetArguments()
+    if Source != '': SSDCache.SetSourceDirectory(Source)
+    if Cache != '': SSDCache.SetCacheDirectory(Cache)
+    SSDCache.RunCLImode()
+    return
+    
+def GetArguments():
     # If there are arguments, parse them for commands:
+    SourcesArgList = ['-s','-src','-source']
+    CacheArgList = ['-c','-cache']
+    SourceDirectory = ''
+    CacheDirectory= ''
     if len(sys.argv) > 1:
         for x in range(1,len(sys.argv)-1):
             if sys.argv[x] in SourcesArgList:
                 SourceDirectory = sys.argv[x+1]
             if sys.argv[x] in CacheArgList:
                 CacheDirectory = sys.argv[x+1]
-    
-    SSDCache.SetCacheDirectory(CacheDirectory)
-    SSDCache.SetSourceDirectory(SourceDirectory)
-    # If prompt for directories if not specified in arguments
-    if SourceDirectory == '': SourceDirectory = SSDCache.PromptSourceDirectory()
-    if CacheDirectory == '': CacheDirectory = SSDCache.PromptCacheDirectory()
-
-    print("Source %s\nCache: %s" % (SourceDirectory,CacheDirectory)) #TESTING
-    for directory in {SourceDirectory, CacheDirectory}:  
-        if exists(directory):
-            if not isdir(directory): 
-                print("ERROR: %s is not a directory" % (directory))
-                return
-        else:
-            #TODO: Prompt user if they would like to create the directory
-            print("ERROR: %s does not exist. Create it using mkdir or a file manager" % (directory))
-            return 
-        
-    if not (SourceDirectory.endswith('/')):
-        SourceDirectory += '/'
-    if not (CacheDirectory.endswith('/')):
-        CacheDirectory += '/'
-    
-    #SSDCache = Cache2SSD(SourceDirectory,CacheDirectory)
-    SourceFilesList = []
-    CachedFilesList = []
-    (SourceFilesList,CachedFilesList) = SSDCache.GetFilesList()
-    if(len(SourceFilesList) == 0 and len(CachedFilesList) == 0):
-        print("Source directory %s is empty, exiting" % (SourceDirectory))
-        return
-    #Display list of choices
-    # Note: Starting the count at 1 for the user, adjusting the users input accordingly
-    # 
-    print("Uncached Directories (Enter number to cache):")
-    for x in range(len(SourceFilesList)):
-        print("%i: %s" % (x+1,SourceFilesList[x])) 
-    print("Cached Directories (Enter number to uncache):")
-    for x in range(len(CachedFilesList)):
-        print("%i: %s" % (len(SourceFilesList)+x+1,CachedFilesList[x]))
-    Choice = int(input("What would you like to do? ->")) -1
-    if Choice < 0 or Choice > len(SourceFilesList)+len(CachedFilesList):
-        print("%i is not a valid choice" % (Choice+1))
-        return
-    elif(Choice < len(SourceFilesList)): # If Choice is among cached
-        SourcePath = SourceDirectory+SourceFilesList[Choice]
-        CachePath = CacheDirectory+SourceFilesList[Choice]
-        if SSDCache.cache(SourcePath,CachePath):
-            print("COMPLETE %s is now cached." % (SourcePath))
-        
-        else :
-            print("ERROR: Not cached.")
-      
-    elif(Choice >= len(SourceFilesList)): # If choice is to uncache
-        Choice -= (len(SourceFilesList))
-        SourcePath = SourceDirectory+CachedFilesList[Choice]
-        CachePath = CacheDirectory+CachedFilesList[Choice]
-
-        if SSDCache.uncache(SourcePath, CachePath):
-            print("COMPLETE %s returned." % (CachePath))
-        else:
-            print("Not cached.")
-    return
-    
-    
-    
-    
+    return (SourceDirectory,CacheDirectory)
 
 if __name__ == "__main__":
     main()
